d4d5d643f57feb685fcaab421369df682e5db4d9
TestBuildGitC# <mr.ekstrimal1432@gmail.com>
2020-02-26 12:54:49 +0200

    zazazazazaz

---
+++ Qvint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Qvint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(((i * 4) % 24 + 1));
+                Console.WriteLine(" ");
+                Console.Write("748347837483 47837483");
+            }
+           
+
+        }
+    }
+}
